267f918ace0043b2bc84b896a52f9b695985078a
sahil kavitake <sahir.kavitake202@gmail.com>
2021-09-29 23:16:26 +0530

    added my profile backend

--- frontend/src/components/Navbar/SidebarScreen.js
+++ frontend/src/components/Navbar/SidebarScreen.js
@@ -11,56 +11,75 @@ import Divider from "@material-ui/core/Divider";
 import PeopleIcon from "@material-ui/icons/People";
 
 import { Link } from "react-router-dom";
-const Style={
- marginTop:"630px",
- backgroundColor:"#c5bfbf",
+
+// backend imports
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+const Style = {
+  marginTop: "630px",
+  backgroundColor: "#c5bfbf",
 };
-const SettingStyle={
-  marginTop:"2px",
- backgroundColor:"#c5bfbf",
+const SettingStyle = {
+  marginTop: "2px",
+  backgroundColor: "#c5bfbf",
 };
 
 function Sidebar() {
+  // *************** Backend Stuff
+
+  let history = useHistory();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push("/login");
+    }
+  }, [history, userInfo]);
+
   return (
     <div>
-        <ListItem button component={Link} to="/" title="Home">
-       <ListItemIcon>
-          <HomeIcon style={{ fill: "purple" }} fontSize="large" />
-        </ListItemIcon> 
-        <ListItemText primary="Home" />
-      </ListItem>
-
-      <ListItem button component={Link} to="/Task" title="TasksScreenUser">
-       <ListItemIcon>
+      <ListItem button component={Link} to="/" title="TasksScreenUser">
+        <ListItemIcon>
           <GroupWorkIcon style={{ fill: "purple" }} fontSize="large" />
-        </ListItemIcon> 
+        </ListItemIcon>
         <ListItemText primary="Tasks" />
       </ListItem>
 
       <ListItem button component={Link} to="/Dashboard" title="Dashboard">
-       <ListItemIcon>
+        <ListItemIcon>
           <DashboardIcon style={{ fill: "purple" }} fontSize="large" />
-        </ListItemIcon> 
+        </ListItemIcon>
         <ListItemText primary="Dashboard" />
       </ListItem>
-      <ListItem button component={Link} to="/users" title="Users">
-       <ListItemIcon>
-          <PeopleIcon style={{ fill: "purple" }} fontSize="large" />
-        </ListItemIcon> 
-        <ListItemText primary="Users List" />
-      </ListItem>
-      
+      {userInfo && userInfo.isAdmin && (
+        <ListItem button component={Link} to="/users" title="Users">
+          <ListItemIcon>
+            <PeopleIcon style={{ fill: "purple" }} fontSize="large" />
+          </ListItemIcon>
+          <ListItemText primary="Users List" />
+        </ListItem>
+      )}
+
       <ListItem style={Style} button component={Link} to="/Help" title="Help">
-      <ListItemIcon>
-          <HelpIcon  fontSize="large" />
-        </ListItemIcon> 
+        <ListItemIcon>
+          <HelpIcon fontSize="large" />
+        </ListItemIcon>
         <ListItemText primary="Help" />
       </ListItem>
 
-      <ListItem style={SettingStyle} button component={Link} to="/Settings" title="Settings">
-       <ListItemIcon>
+      <ListItem
+        style={SettingStyle}
+        button
+        component={Link}
+        to="/Settings"
+        title="Settings"
+      >
+        <ListItemIcon>
           <SettingsIcon fontSize="large" />
-        </ListItemIcon> 
+        </ListItemIcon>
         <ListItemText primary="Settings" />
       </ListItem>
     </div>
